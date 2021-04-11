64542678f0a20fb335182614ecfa4df8e8a32d90
Abdelaziz BENJELLOUN <abenjelloun@kds.com>
2021-04-11 14:03:58 +0200

    clean db at loading time

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ db.mongoose
     process.exit();
   });
 
+// clean db
+const Meal = db.meals;
+Meal.collection.drop();
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to abenjelloun application." });
@@ -42,19 +46,19 @@ require("./app/routes/meal.routes")(app);
 // load csv
 const csv = require('csv-parser');
 const fs = require('fs');
-const Meal = db.meals;
 
 fs.createReadStream('./app/resources/ciqual_20200707.csv', {encoding: 'utf-8'})
   .pipe(csv({ separator: ';' }))
   .on('data', (row) => {
-    console.log(row);
+    
     // Create a Meal
     const meal = new Meal({
       code: row.alim_code,
       title: row.alim_nom_fr,
-      carbo: row.carbo, //typeof carbo == "number" ? Number.parseFloat(carbo) : 0,
+      carbo: row.carbo,
       isReference: true
     });
+
     // Save Meal in the database
     meal
       .save(meal)
@@ -63,33 +67,8 @@ fs.createReadStream('./app/resources/ciqual_20200707.csv', {encoding: 'utf-8'})
       });
   })
   .on('end', () => {
-    console.log('CSV file successfully processed');
+    console.log('Csv file successfully processed!');
   });
-
-// init db meals
-/*
-const Meal = db.meals;
-var referenceMeals = require("./app/resources/reference-meals.json");
-for(var i in referenceMeals) {
-  var referenceMeal = referenceMeals[i];
-  /*var carbo = typeof referenceMeal.carbo == "string" && referenceMeal.carbo.indexOf('$') > -1
-              ? referenceMeal.carbo.replace(',', '.')
-              : referenceMeal.carbo;
-  // Create a Meal
-  const meal = new Meal({
-    code: referenceMeal.alim_code,
-    title: referenceMeal.alim_nom_fr,
-    carbo: referenceMeal.carbo, //typeof carbo == "number" ? Number.parseFloat(carbo) : 0,
-    isReference: true
-  });
-  // Save Meal in the database
-  meal
-    .save(meal)
-    .catch(err => {
-      console.log("Some error occurred while creating the Meal.");
-    });
-}
-*/
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
